458e59ee6272fdfabecaaaa4410cdc2c3852606f
NicollyOliveiraS <nicolly.oliveira.senai@gmail.com>
2026-03-27 10:40:44 -0300

    valorde0

--- ex2/script.js
+++ ex2/script.js
@@ -1,8 +1,11 @@
 document.getElementById('calcular').addEventListener('click', function() {
             const salarioInput = document.getElementById('salario');
             const salario = parseFloat(salarioInput.value);
+            if (salario <= 0) {
+                alert('O salário deve ser maior que zero.');
+                return;
+            }
             let aumento = 0;
-
             if (salario <= 1200) {
                 aumento = salario * 0.16;
             } else if (salario > 1200 && salario <= 2100) {
@@ -12,7 +15,7 @@ document.getElementById('calcular').addEventListener('click', function() {
             } else if (salario > 3000) {
                 aumento = salario * 0.05;
             }
-
+            
             const novoSalario = salario + aumento;
             document.getElementById('resultado').textContent = `Novo salário: R$ ${novoSalario.toFixed(2)}`;
         });
